d9b2b33ae7cf22ed2da6e1ef8353f35338bab5f2
Hunter Bertoson <44106297+hkbertoson@users.noreply.github.com>
2024-07-05 10:59:30 -0500

    Bug fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
 	integrations: [mdx(), sitemap()],
-	output: 'static',
+	output: 'hybrid',
 	adapter: cloudflare({
 		imageService: 'cloudflare',
 	}),
